c4cf64b87d3383574289799d0135f47dcfac154f
Lukeveille <luke@lukeleveille.com>
2019-08-26 00:12:34 -0400

    basic index display showing event names

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
-export default () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+    
+import fetch from 'isomorphic-unfetch';
+import Router from 'next/router';
+import { useState } from 'react';
+import { handleAuthSSR } from '../utils/auth';
+
+const Index = props => {
+  const stuff = Object.values(props),
+  [events, setEvents] = useState(stuff.slice(0, stuff.length-1) || []);
+
+  console.log(events)
+
+  return (
+    <div>
+      <h1>Events</h1>
+      {events.map(event => {
+        return <p key={event.id}>{event.name}</p>
+      })}
+    </div>
+  )
+};
+
+Index.getInitialProps = async function (ctx) {
+  const [headers, server] = handleAuthSSR(ctx);
+  try {
+    const res = await fetch(server + 'events', headers);
+    return await res.json();
+  } catch (err) {
+    console.error(err)
+    if (ctx.res) {
+      ctx.res.writeHead(302, {
+        Location: '/'
+      });
+      ctx.res.end();
+    } else {
+      Router.push('/');
+    };
+  };
+};
+
+export default Index;
---
+++ utils/auth.js
@@ -0,0 +1,17 @@
+
+import { Cookies } from 'react-cookie';
+
+const server = 'http://localhost:3443/api/v1/';
+const cookies = new Cookies();
+
+export const handleAuthSSR = (ctx) => {
+  let token = null;
+  
+  if (ctx && ctx.req && ctx.req.headers.cookie) {
+    token = ctx.req.headers.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  } else {
+    token = cookies.get('auth_token');
+  };
+
+  return [{ headers: { Authorization: 'Bearer ' + token } }, server];
+};
